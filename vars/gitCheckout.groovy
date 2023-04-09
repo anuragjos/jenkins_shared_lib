0918bad06445104cc8d19094e902a3fac2e57e1f
anurag joshi <joshianurag088@gmail.com>
2023-04-09 11:48:53 +0530

    git checkout

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,9 @@
-def call(Map stageParams){
-    checkout{[
+/* groovylint-disable-next-line NoDef */
+/* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
+def call(Map stageParams) {
+    checkout {[
         $class: 'GitSCM',
         branches: [[ name: stageParams.branch ]],
-        userRemoteConfigs: [[url: stageParams.url]]
+        userRemoteConfigs: [[ url: stageParams.url ]]
     ]}
 }

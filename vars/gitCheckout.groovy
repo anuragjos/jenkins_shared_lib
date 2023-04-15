e67b7eed09fdc629c8378f433456ce4c405d7efb
anuragjos <88484101+anuragjos@users.noreply.github.com>
2023-04-15 17:27:29 +0530

    Delete vars directory

--- vars/gitCheckout.groovy
+++
@@ -1,9 +0,0 @@
-/* groovylint-disable-next-line NoDef */
-/* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
-def call(Map stageParams) {
-    checkout ([
-        $class: 'GitSCM',
-        branches: [[ name: stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-}

c1eae5a4b6e0bcc2667c2e988f6309d24968d543
Anurag Joshi <joshianurag088@gmail.com>
2023-06-13 19:25:44 +0530

    image push

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,11 +1,11 @@
 def call(String project, String ImageTag, String hubUser){
     withCredentials([usernamePassword(
         credentialsId: 'docker', 
-        passwordVariable: 'PASS', 
-        usernameVariable: 'USER'
+        usernameVariable: 'USER',
+        passwordVariable: 'PASS'
         )]) {
             
-            sh "docker login -u '$USER' -P '$PASS'"
+            sh "docker login -u '$USER' -p '$PASS'"
         }
         sh "docker image push ${hubUser}/${project}:${ImageTag}"
         sh "docker image push ${hubUser}/${project}:latest"

cbd0640c93d3172a714d5086c88b69838c8277be
Anurag joshi <joshianurag088@gmail.com>
2026-04-29 18:48:47 +0530

    docker

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(String project, String Imagetag, String hubUser) {
-    bat "docker image build -t ${hubUser}:${project} ."
-    bat "docker image tag ${hubUser}/${project} ${hubUser}/${project}:${Imagetag}"
-    bat "docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest"
+    bat "docker build -t ${hubUser}/${project}:${Imagetag} ."
+    bat "docker image tag ${hubUser}/${project}:${Imagetag} ${hubUser}/${project}:latest"
+    bat "docker images"
 }
